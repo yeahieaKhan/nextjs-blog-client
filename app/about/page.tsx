f2cbc1002d65290b0512be9a78d44d66aaa549de
yeahieakhan <yeahieakhan2017@gmail.com>
2026-04-17 22:01:26 +0600

    done loading

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,4 +1,6 @@
-const AboutPage = () => {
+const AboutPage = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 4000));
+
   return <div>this is AboutPage page</div>;
 };
 export default AboutPage;
