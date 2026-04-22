d61094602e5ae83f4c25769b30a1a8a1ebb36cbc
yeahieakhan <yeahieakhan2017@gmail.com>
2026-04-22 17:58:56 +0600

    updated

--- app/(CommonLayout)/page.tsx
+++ app/(CommonLayout)/page.tsx
@@ -1,4 +1,8 @@
-export default function Home() {
+import { authClient } from "@/lib/auth-client";
+
+export default async function Home() {
+  const session = await authClient.getSession();
+  console.log(session);
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       this is home page
