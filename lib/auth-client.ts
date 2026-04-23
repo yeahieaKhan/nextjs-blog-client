a3af81e5301d2def9a229588eb1457b5afef0c76
yeahieakhan <yeahieakhan2017@gmail.com>
2026-04-23 17:12:29 +0600

    google auth done

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,4 +1,5 @@
 import { createAuthClient } from "better-auth/react";
+
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:5000",
+  baseURL: "http://localhost:8080", // 🔥 MUST FIX
 });
