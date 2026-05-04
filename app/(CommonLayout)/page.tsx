2c39f577a94771e7db29bffacddbadb1c4c7b9bd
yeahieakhan <yeahieakhan2017@gmail.com>
2026-05-04 17:59:50 +0600

    done 32

--- app/(CommonLayout)/page.tsx
+++ app/(CommonLayout)/page.tsx
@@ -1,5 +1,6 @@
 import BlogCard from "@/components/modules/Homepages/BlogCard";
 import { blogService } from "@/services/blog.service";
+import { BlogPost } from "@/type";
 
 export default async function Home() {
   const res = await blogService.getAllPost();
@@ -10,7 +11,7 @@ export default async function Home() {
 
   return (
     <div className="w-full max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 px-4 py-8">
-      {posts?.map((post) => (
+      {posts?.map((post: BlogPost) => (
         <BlogCard key={post.id} post={post} />
       ))}
     </div>
--- services/blog.service.ts
+++ services/blog.service.ts
@@ -3,11 +3,12 @@ const API_URL = process.env.BACKEND_API;
 export const blogService = {
   getAllPost: async function () {
     try {
-      const res = await fetch(`${API_URL}/post`);
+      const res = await fetch(`${API_URL}/post`, { next: { revalidate: 10 } });
       const data = await res.json();
 
       return { data: data, error: null };
     } catch (error) {
+      console.log(error);
       return { data: null, error: { message: "Something went worng" } };
     }
   },
